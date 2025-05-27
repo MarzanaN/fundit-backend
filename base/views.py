b66ceaf0e6b463f88c78b820f97a1a5ebf1eeabd
MarzanaN <m.nahar@live.co.uk>
2025-05-27 21:26:22 +0100

    Added build fodler

--- base/views.py
+++ base/views.py
@@ -49,7 +49,7 @@ User = get_user_model()
 class FrontendAppView(View):
     def get(self, request):
         try:
-            with open(os.path.join(settings.BASE_DIR, '../fundit-react-app/build', 'index.html')) as f:
+            with open(os.path.join(settings.BASE_DIR, 'build', 'index.html')) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
             return HttpResponse(
