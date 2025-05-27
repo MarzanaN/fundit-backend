706174e62148d6e2ce69ad67500dd8c9f3c54b65
MarzanaN <m.nahar@live.co.uk>
2025-05-27 22:59:53 +0100

    urls changed

--- fundit_backend/urls.py
+++ fundit_backend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from base.views import FrontendAppView  
+from django.conf import settings
+from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +11,8 @@ urlpatterns = [
     path('', FrontendAppView.as_view()),  
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=os.path.join(settings.BASE_DIR, 'fundit_backend', 'build', 'static')
+    )
