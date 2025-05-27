ca8b1f3a144e3901cf12e1de5d71df7d4ac8168b
MarzanaN <m.nahar@live.co.uk>
2025-05-27 22:02:18 +0100

    please work im going to cry

--- fundit_backend/settings.py
+++ fundit_backend/settings.py
@@ -129,12 +129,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'build', 'static'), 
+    os.path.join(BASE_DIR, 'build', 'static'),
 ]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, '..', 'build')]  
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'build')]
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- fundit_backend/urls.py
+++ fundit_backend/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from base.views import FrontendAppView
+from django.urls import path, include
+from base.views import FrontendAppView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
-    re_path(r'^.*$', FrontendAppView.as_view()),
+    path('api/', include('base.urls')),  
+    path('', FrontendAppView.as_view()),  
 ]
-
 
 
